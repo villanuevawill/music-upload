d1078e4dc47174cb171df037855f9b2b02f769b7
Will <wvillanu89@gmail.com>
2015-05-06 22:26:34 -0700

    Updated to use browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util'),
     concat = require('gulp-concat');
 
 
@@ -11,10 +17,32 @@ gulp.task('serve', function() {
     });
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('./client/public/**/*.js')
-    .pipe(concat('all.js'))
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './entry.js',
+    debug: true
+  });
+
+// browserify task
+gulp.task('browserify', function() {
+  return browserify('./cleint/public/main.js')
+      .bundle()
+      //Pass desired output filename to vinyl-source-stream
+      .pipe(source('bundle.js'))
+      // Start piping stream to tasks!
+      .pipe(gulp.dest('./dist/'));
+});
+
+  return b.bundle()
+    .pipe(source('./client/public/**/*.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./client/dist/'));
 });
 
-gulp.task('default', ['scripts', 'serve'])
+gulp.task('default', ['browserify', 'serve'])
